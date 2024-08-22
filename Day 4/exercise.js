fd9089beefd6ad53dcb5b435a14378f51071913b
rence <99105241+rencejke@users.noreply.github.com>
2024-08-22 22:28:59 +0800

    almost donee

--- Day 4/exercise.js
+++ Day 4/exercise.js
@@ -27,7 +27,7 @@ if(myAge > yourAge)
     console.log(`Your ${yourAge - myAge} years older than me`)
 }
 
-//2 
+//3
 
 let a = 4
 let b = 3
@@ -46,7 +46,7 @@ else
 a > b ? console.log('a is greater than b') :  console.log('a is less than b')
 
 
-//3
+//4
 let num = Number(prompt('Enter Number'))
 let even = num % 2
 
@@ -56,4 +56,81 @@ if (even === 0)
 }else{
     console.log(`${num} is an odd number`)
 }
+
+//exercise level 2
+
+//1
+let grade = Number(prompt('Enter Your Grade'))
+
+switch(true)
+{
+    case grade >=80 && grade <= 100:
+        console.log('A')
+    break;
+
+    case grade >=70 && grade <= 89:
+        console.log('B')
+    break;
+
+    case grade >=60 && grade <= 69:
+        console.log('C')
+    break;
+
+    case grade >=50 && grade <= 59:
+        console.log('D')
+    break;
+
+    case grade >=0 && grade <= 49:
+        console.log('F')
+    break;
+    
+    default:
+        console.log('Error')
+}
+
+//2
+
+let season = prompt('Enter Your Season').toLowerCase()
+
+switch(true)
+{
+    case season == 'september' || season == 'october' || season == 'november':
+        console.log('Autumn')
+    break;
+
+    case season == 'december' || season == 'january' || season == 'february':
+        console.log('Winter')
+    break;
+
+    case season == 'march' || season == 'april' || season == 'may':
+        console.log('Spring')
+    break;
+
+    case season == 'june' || season == 'july' || season == 'august':
+        console.log('Summer')
+    break;
+    
+    default:
+        console.log('Error')
+}
+
+
+//3
+
+let days =  prompt('What is the day today').toLowerCase()
+
+if(days == 'saturday' ||  days == 'sunday')
+{
+    console.log(`${days} is a weekend`)
+}
+else if(days == 'monday' ||  days == 'tuesday' || days == 'wednesday' || days == 'thursday' || days == 'friday')
+{
+    console.log(`${days} is a working day`)
+}
+
+//4
+
+
+
+
 
