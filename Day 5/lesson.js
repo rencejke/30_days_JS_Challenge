ea447fa23d446832ffd7d37d2431f63d9921e363
rence <99105241+rencejke@users.noreply.github.com>
2024-08-28 22:44:44 +0800

    start day 5

---
+++ Day 5/lesson.js
@@ -0,0 +1,18 @@
+//Methods to manipulate array
+
+
+//creates an array
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
+
+
+
+//Creating static values with fill
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+
