acf4763445ac6ec294f7c44dc01b39a8331e3b14
rence <99105241+rencejke@users.noreply.github.com>
2024-10-09 04:02:51 +0800

    updated

---
+++ Day 6/d6-exercise.js
@@ -0,0 +1,22 @@
+
+
+for(let i = 0; i <= 10; i++)
+    {
+        console.log(i)
+    } //0 1 2 3 4 5 6 7 8 9 10
+
+    let count = 0
+    while( count <= 10 )
+    {
+        console.log(count)
+        count++
+    } //0 1 2 3 4 5 6 7 8 9 10
+
+let count2 = 0
+do
+{
+    console.log(count)
+    count2++
+}while(count2 <= 10)
+    //0 1 2 3 4 5 6 7 8 9 10 
+
--- Day 6/lesson.js
+++ Day 6/lesson.js
@@ -46,9 +46,93 @@ for(let i = 0; i < numbers.length; i++){
 //Creating a new array based on the existing array
  const numbers2 = [1, 2, 3, 4, 5]
  const newArr2 = []
- let sum2 = 0
  for(let i = 0; i < numbers2.length; i++){
    newArr2.push( numbers2[i] ** 2)
  }
 
  console.log(newArr2)  // [1, 4, 9, 16, 25]
+
+//while loop
+let k = 0
+while (k <= 5) {
+  console.log(i)
+  k++
+} // 0 1 2 3 4 5
+
+ //do while
+ let l = 0
+do {
+  console.log(l)
+  l++
+} while (l <= 5)
+
+// 0 1 2 3 4 5
+
+//for of loops
+const numbers3 = [1, 2, 3, 4, 5]
+
+for (const num of numbers3) {
+  console.log(num)
+}
+
+// 1 2 3 4 5
+
+for (const num of numbers3) {
+  console.log(num * num)
+}
+
+// 1 4 9 16 25
+
+const countries2 = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+const newArr3 = []
+for(const country of countries2){
+  newArr3.push(country.toUpperCase())
+}
+
+console.log(newArr3)  // ["FINLAND", "SWEDEN", "NORWAY", "DENMARK", "ICELAND"]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+for (const tech of webTechs) {
+  console.log(tech.toUpperCase())
+}
+
+// HTML CSS JAVASCRIPT REACT NODE MONGODB
+
+for (const tech of webTechs) {
+  console.log(tech[0]) // get only the first letter of each element,  H C J R N M
+}
+
+//break
+//Break is used to interrupt a loop.
+
+for(let i = 0; i <= 5; i++){
+  if(i == 3){
+    break
+  }
+  console.log(i)
+} // 0 1 2
+
+
+//continue
+//We use the keyword continue to skip a certain iterations.
+
+for(let i = 0; i <= 5; i++){
+  if(i == 3){
+    continue
+  }
+  console.log(i)
+}
+
+// 0 1 2 4 5
+
+
+
