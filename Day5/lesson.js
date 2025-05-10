328653f15bb20e6f916b5720606da3e8f5462af1
rencejke <bonillaclarencejake@gmail.com>
2025-05-11 01:55:38 +0800

    Day 5 is still in progress

--- Day5/lesson.js
+++ Day5/lesson.js
@@ -47,3 +47,96 @@ const countries = [
 
   //Methods to manipulate array
   //There are different methods to manipulate an array. These are some of the available methods to deal with arrays:Array, length, concat, indexOf, slice, splice, join, toString, includes, lastIndexOf, isArray, fill, push, pop, shift, unshift
+
+const arr3 = Array() // creates an an empty array
+console.log(arr3)
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
+
+
+// .fill() ---Fill all the array elements with a static value
+
+const arr4 = Array() // creates an an empty array
+console.log(arr4)
+
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+
+const four4values = Array(4).fill(4) // it creates 4 element values filled with '4'
+console.log(four4values) // [4, 4, 4, 4]
+
+
+// .concat() --To concatenate two arrays.
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+
+
+
+// Length:To know the size of the array
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.length) // -> 5 is the size of the array
+
+//indexOf:To check if an item exist in an array. If it exists it returns the index else it returns -1.
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.indexOf(5)) // -> 4
+console.log(numbers.indexOf(0)) // -> -1
+console.log(numbers.indexOf(1)) // -> 0
+console.log(numbers.indexOf(6)) // -> -1
+
+
+// lcheck if a banana exist in the array
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+let index = fruits.indexOf('banana')  // 0
+
+if(index === -1){
+   console.log('This fruit does not exist in the array')  
+} else {
+    console.log('This fruit does exist in the array')
+}
+// This fruit does exist in the array
+
+// we can use also ternary here
+index === -1 ? console.log('This fruit does not exist in the array') : console.log('This fruit does exist in the array')
+
+//Getting last index of an element in array
+//lastIndexOf: It gives the position of the last item in the array. If it exist, it returns the index else it returns -1.
+
+const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+
+console.log(numbers.lastIndexOf(2)) // 7
+console.log(numbers.lastIndexOf(0)) // -1
+console.log(numbers.lastIndexOf(1)) //  6
+console.log(numbers.lastIndexOf(4)) //  3
+console.log(numbers.lastIndexOf(6)) // -1
+
+
+//includes:To check if an item exist in an array. If it exist it returns the true else it returns false.
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.includes(5)) // true
+console.log(numbers.includes(0)) // false
+console.log(numbers.includes(1)) // true
+console.log(numbers.includes(6)) // false
+
+
+//Checking array
+//Array.isArray:To check if the data type is an array
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(Array.isArray(numbers)) // true
+
+const number = 100
+console.log(Array.isArray(number)) // false
+
+
