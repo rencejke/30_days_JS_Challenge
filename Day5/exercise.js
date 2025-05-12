389be9cb56b4c292a72e18cb88ff4021107a73b3
rencejke <bonillaclarencejake@gmail.com>
2025-05-12 08:56:05 +0800

    Day 5 in progress

--- Day5/exercise.js
+++ Day5/exercise.js
@@ -1,1 +1,28 @@
-//in progress.....
+//Exercise: Level 1
+
+//1 Declare an empty array;
+const empty = new Array()
+
+//2 Declare an array with more than 5 number of elements
+const countries = ['United States', 'Denmark', 'France', 'Japan', 'Finland', 'Estoria']
+
+//3 Find the length of your array
+console.log(countries.length) //6
+
+//4 Get the first item, the middle item and the last item of the array
+console.log(countries[0]) //United States
+console.log(countries[2]) //France
+console.log(countries[5]) //Estoria
+
+//5 Declare an array called mixedDataTypes, 
+// put different data types in the array and find the length of the array. The array size should be greater than 5
+
+const mixedDataTypes = ['Kim Chaewon', 24, {firstGroup: 'IZONE', secondGroup:'LE SSERAFIM'},
+                        'August 1, 2000', 'Seoul, Korea', 'She like manndarins', 'Favorite Fruit is Mandarin',
+                        {Height: '164cm (5\'4)', Weight: '42kg (92 lbs)'}]
+                        
+console.log(mixedDataTypes.length) //8
+
+
+
+
--- Day5/lesson.js
+++ Day5/lesson.js
@@ -139,4 +139,138 @@ console.log(Array.isArray(numbers)) // true
 const number = 100
 console.log(Array.isArray(number)) // false
 
+//Converting array to string
+//toString:Converts array to string
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+
+//Joining array elements
+//join: It is used to join the elements of the array, the argument we passed in the join method will be joined in the array and return as a string.
+
+//.join() -- it adds space, text, symbols after each element
+
+const names2 = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+console.log(names2.join()) // Asabeneh,Mathias,Elias,Brook
+console.log(names2.join('')) //AsabenehMathiasEliasBrook
+console.log(names2.join(' ')) //Asabeneh Mathias Elias Brook
+console.log(names2.join(', ')) //Asabeneh, Mathias, Elias, Brook
+console.log(names2.join(' # ')) //Asabeneh # Mathias # Elias # Brook
+
+
+//Slice 
+//Slice: To cut out a multiple items in range. It takes two parameters:starting and ending position. It doesn't include the ending position.
+
+  const numbers3 = [1,2,3,4,5]
+
+  console.log(numbers3.slice()) // -> it copies all  item
+  console.log(numbers3.slice(0)) // -> it copies all  item
+  console.log(numbers3.slice(0, numbers3.length)) // it copies all  item
+  console.log(numbers3.slice(1,4)) // -> [2,3,4] // it doesn't include the ending position
+
+
+//Splice method in array
+//Splice: It takes three parameters:Starting position, number of times to be removed and number of items to be added.
+
+  const numbers = [1, 2, 3, 4, 5]
+  numbers.splice()
+  console.log(numbers)                // -> remove all items
+  
+  const numbers = [1, 2, 3, 4, 5]
+  numbers.splice(0,1)
+  console.log(numbers)            // remove the first item
+
+  const numbers = [1, 2, 3, 4, 5, 6]
+  numbers.splice(3, 3, 7, 8, 9)
+  console.log(numbers.splice(3, 3, 7, 8, 9))  // -> [7, 8, 9] //it removes three item and replace three items
+// first 3 is the starting position, the second 3 is the number of times to be remove so in the array [1, 2, 3, 4, 5, 6]
+//4,5,6 will be remove later on and 7,8,9 will be the one to be added
+
+//Add an item to the array using push 
+//.push('item to be added')
+
+const arr5 = ['item1', 'item2','item3']
+arr.push('new item')
+console.log(arr5)
+// ['item1', 'item2','item3','new item']
+
+//Removing the end element using pop
+//pop: Removing item in the end. 
+
+//.pop()
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.pop() // -> remove one item from the end
+console.log(numbers) // -> [1,2,3,4]
+
+//Removing an element from the beginning
+//shift: Removing one array element in the beginning of the array.
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.shift() // -> remove one item from the beginning
+console.log(numbers) // -> [2,3,4,5]
+
+
+//Add an element from the beginning
+//unshift: Adding array element in the beginning of the array.
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.unshift(0) // -> add one item from the beginning
+console.log(numbers) // -> [0,1,2,3,4,5]
+
+//Reversing array order
+//reverse: reverse the order of an array.
+// its like descending order 
+
+const numbers = [1, 2, 3, 4, 5]
+numbers.reverse() // -> reverse array order
+console.log(numbers) // [5, 4, 3, 2, 1]
+
+//Sorting elements in array
+//sort: arrange array elements in ascending order.
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+webTechs.sort()
+console.log(webTechs) // ["CSS", "HTML", "JavaScript", "MongoDB", "Node", "React", "Redux"]
+
+//Array of arrays
+//Array can store different data types including an array itself.
+
+ const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+ const backEnd = ['Node','Express', 'MongoDB']
+ const fullStack = [frontEnd, backEnd]
+ console.log(fullStack)   // [["HTML", "CSS", "JS", "React", "Redux"], ["Node", "Express", "MongoDB"]]
+ console.log(fullStack.length)  // 2
+ console.log(fullStack[0])  // ["HTML", "CSS", "JS", "React", "Redux"]
+ console.log(fullStack[1]) // ["Node", "Express", "MongoDB"]
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
