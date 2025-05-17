7a44fdd895ea7ed0a60aa8493658c5e2e856a97d
rencejke <bonillaclarencejake@gmail.com>
2025-05-17 22:30:46 +0800

    Day 5 still in progress

--- Day5/exercise.js
+++ Day5/exercise.js
@@ -156,6 +156,289 @@ else
     console.log(countries)
 }
 
+//5
+//import { webTechs } from './web_techs.js';
+//import { countries } from './countries.js';
+
+if(webTechs.includes('Sass'))
+{
+    console.log('Sass is a CSS preprocess')
+}
+else
+{
+   webTechs.push('Sass')
+   console.log(webTechs) 
+}
+//output
+/*
+[
+  'HTML',       'CSS',
+  'JavaScript', 'React',
+  'Redux',      'Node',
+  'MongoDB',    'Sass'
+]
+*/
+//6 
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+
+const fuillStack = frontEnd.concat(backEnd)
+ console.log(fuillStack) 
+ //output
+/*
+[
+  'HTML',    'CSS',
+  'JS',      'React',
+  'Redux',   'Node','Express', 'MongoDB'
+]
+*/
+
+//Exercise Level 3
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+//1
+console.log(ages.sort()) //[19, 19, 20, 22, 24, 24, 24, 25, 25, 26]
+
+const minAge = Math.min(...ages); // ...spread operator
+const maxAge = Math.max(...ages);
+
+console.log(minAge); // 19
+console.log(maxAge); // 26
+
+//2
+const middle1 = ages[ages.length / 2 - 1]
+const middle2 = ages[ages.length / 2]
+
+console.log([middle1, middle2]) //20 25 if not sorted if sorted 24 24
+
+//3 
+//const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+const sum = ages.reduce((total, num) => total + num, 0);
+let average = sum / ages.length
+console.log(average) //22.8 average age
+
+//4
+//const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+//const minAge = Math.min(...ages); // ...spread operator
+//const maxAge = Math.max(...ages);
+
+//const minAge = Math.min(...ages); 
+//const maxAge = Math.max(...ages);
+
+console.log(maxAge - minAge) //7 range of the ages
+
+//5
+console.log(Math.abs(minAge - maxAge)) 
+
+const countries2 = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+]
+  
+  //Slice the first ten countries from the countries array
+  const findCountries = Math.ceil(countries2.length / 19)  //10
+  console.log(countries2.slice(0, findCountries)) 
+
+  //output
+  /*['Afghanistan', 'Albania', 'Algeria','Andorra','Angola', 
+  'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria','Azerbaijan'] */
+
+
+
+
+
+
 
 
 
