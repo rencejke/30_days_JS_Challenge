099e7329f133608f8b2088647cce2723beb1b1a5
rencejke <bonillaclarencejake@gmail.com>
2025-05-25 15:50:01 +0800

    Day 6 in progress

--- Day6/exercise.js
+++ Day6/exercise.js
@@ -117,6 +117,93 @@ do{
 //9 Use for loop to iterate from 0 to 100 and print only prime numbers
 
 
+for (let i = 2; i < 100; i++) { //checking from 2 because 0 and 1 are not prime.
+    let isPrime = true;
+    
+    for (let j = 2; j <= Math.sqrt(i); j++) { //For each number i, it checks if it’s divisible by any number j from 2 up to the square root of i.
+        
+        if (i % j === 0) { //If it’s divisible, it sets isPrime to false.
+            isPrime = false;
+            break;
+        }
+    }
+    
+    if (isPrime) { // isPrime stays true, it prints the number 
+        console.log(i);
+    }
+}
+
+//10 Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+
+let sum = 0
+
+for(let i = 0; i <= 100; i++)
+{
+    sum += i
+}
+console.log(`The sum of all numbers from 0 to 100 is ${sum}`) //5050
+
+
+//11
+let sumEven = 0
+let sumOdd = 0
+for(let i = 0; i <= 100; i++)
+{
+    if(i % 2 === 0)
+    {
+        sumEven += i
+    }
+}
+
+for(let i = 0; i <= 100; i++)
+{
+    if(i % 2 != 0)
+    {
+        sumOdd += i
+    }
+}
+console.log(`The sum of all evens from 0 to 100 is ${sumEven}. And the sum of all odds from 0 to 100 is ${sumOdd}.`)
+
+//12
+const array = new Array()
+array.unshift(sumEven)
+array.push(sumOdd)
+console.log(array) //[2550, 2500]
+
+
+//13 Develop a small script which generate array of 5 random numbers
+
+let array2 = new Array()
+
+for(let i = 0; i < 5; i++)
+{
+    const randomNum = Math.floor(Math.random () * 6)
+    array2.push(randomNum)
+}
+console.log(array2)
+
+
+//14 Develop a small script which generate array of 5 random numbers and the numbers must be unique
+let array3 = new Array()
+
+for(let i = 0; i < 5; i++)
+{
+    const randomNum2 = Math.floor(Math.random () * 6)
+    if(!array3.includes(randomNum2))
+    {
+        array3.push(randomNum2)    
+    }
+}
+console.log(array3)
+
+//15 Develop a small script which generate a six characters random id:
+
+for(let i = 0; i <= 6; i++)
+{
+       
+}
+
+
 
 
 
